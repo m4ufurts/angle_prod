eb25a22cd63ab49561c461e53746d4a7c69de8d5
Walace Leal <walaceleal.nave@gmail.com>
2022-01-06 01:51:27 -0300

    configuração do mysql com sgbd e ajuste dos arquivos ignorados

--- project/settings.py
+++ project/settings.py
@@ -78,6 +78,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+            'read_default_file': str(BASE_DIR / '.env'),
+        },
+    },
+    'sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
